cf99385ff5911d9d07b73043208b00646ccd6dfd
MichelySerras <michelysrrs@gmail.com>
2024-12-05 20:24:00 -0300

    Adicionei métodos na função Produto Service

--- Service/ProdutoService.cs
+++ Service/ProdutoService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using SistemaDeVendas.Repository;
+using SistemaDeVendas.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace SistemaDeVendas.Service
 {
     internal class ProdutoService
     {
-    }
+        private ProdutoRepository produtoRepo = ProdutoRepository.getInstance();
+
+        public void adicionar(String marca, String modelo, String descricao, Double preco)
+        {
+            Produto produto = new Produto(marca, modelo, descricao, preco)
+            try
+            {
+                produtoRepo.adicionar(produto);
+            }catch (Exception ex) {
+                throw new Exception("Não foi possível cadastrar o produto!");
+            }
+        }
+
+        public void listar(Dictionary<int, Produto> lista) { 
+            
+        }
+
+        public Produto remover(int id)
+        {
+
+        }
+
+        public Produto buscarPorId(int id) {
+
+        }
 }
