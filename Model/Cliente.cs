160b0f7f49794188531d50f4caf6378ef6ecdf45
MaluAbrami <marialuizaabrami@gmail.com>
2024-12-05 19:41:10 -0300

    add model de cliente

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -8,5 +8,66 @@ namespace SistemaDeVendas.Model
 {
     internal class Cliente
     {
+        private int codigo;
+        private string nome;
+        private int idade;
+        private string cpf;
+
+        public Cliente(string nome, int idade, string cpf)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.cpf = cpf;
+        }
+
+        public int Codigo
+        {
+            get 
+            { 
+                return codigo; 
+            }
+            set
+            {
+                if (value < 0) return;
+                else this.codigo = value;   
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                this.nome = value;
+            }
+        }
+
+        public int Idade
+        {
+            get
+            {
+                return idade;
+            }
+            set
+            {
+                if (value <= 0) return;
+                else this.idade = value;
+            }
+        }
+
+        public String Cpf
+        {
+            get
+            {
+                return cpf;
+            }
+            set
+            {
+                this.cpf = value;
+            }
+        }
     }
 }
